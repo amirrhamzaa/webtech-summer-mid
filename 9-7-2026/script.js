5996b5d30894d22801eb5e44dc73341cf0258816
amirrhamzaa <23-54816-3@aiub.edu>
2026-07-15 23:26:45 +0600

    Updated

---
+++ 9-7-2026/script.js
@@ -0,0 +1,42 @@
+let totalCount = 0;
+
+function calculateForm() {
+  const fName = document.getElementById("firstName").value;
+  const email = document.getElementById("email").value;
+
+  let hasNameError = true;
+  let hasEmailError = true;
+  if (!name) {
+    document.getElementById("fnameError").innerHTML = "Name can not be empty";
+    document.getElementById("fnameError").style.color = "red";
+    hasNameError = true;
+  } else if (name.length < 3) {
+    document.getElementById("fnameError").innerHTML =
+      "Name must be at least 3 char";
+    document.getElementById("fnameError").style.color = "red";
+    hasNameError = true;
+  } else {
+    document.getElementById("fnam").innerHTML = "";
+    hasNameError = false;
+  }
+
+  if (!email) {
+    document.getElementById("emailError").innerHTML =
+      "Email is a required fireld";
+    document.getElementById("emailError").style.color = "red";
+    hasEmailError = true;
+  } else if (!email.includes("@")) {
+    document.getElementById("emailError").innerHTML =
+      "Please provide a valid email address";
+    document.getElementById("emailError").style.color = "red";
+    hasEmailError = true;
+  } else {
+    document.getElementById("emailError").innerHTML = "";
+    hasEmailError = false;
+  }
+
+  if (!hasNameError && !hasEmailError) {
+    document.getElementById("total").innerHTML = ++totalCount;
+  }
+  return false;
+}
