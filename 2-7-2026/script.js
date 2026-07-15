968675f3f7604e2510f9a865e033af18fa1bd3be
amirrhamzaa <23-54816-3@aiub.edu>
2026-07-16 05:19:59 +0600

    practice complete.

--- 2-7-2026/script.js
+++ 2-7-2026/script.js
@@ -1,46 +1,48 @@
 console.log("connected")
 
 function validateForm(){
-    const fName = document.getElementById("firstName").value;
-    const lName = document.getElementById("lastName").value;
-    const email = document.getElementById("email").value;
-    const phone = document.getElementById("phone").value;
 
-    console.log(fName, lName, email, phone);
+const fName = document.getElementById("firstName").value;
+const lName = document.getElementById("lastName").value;
+const email = document.getElementById("email").value;
+const phone = document.getElementById("phone").value;
 
-        if(!fName){
-        document.getElementById("fnameError").innerHTML = "First Name is required";
-    }else if(fName.length < 4){
-    document.getElementById("fnameError").innerHTML = "First Name should be at least 4 char long";
-    }else{
-        document.getElementById("fnameError").innerHTML ="";
-    }
-           if(!lName){
-        document.getElementById("lnameError").innerHTML = "Last Name is required";
-    }else if(fName.length < 4){
-    document.getElementById("lnameError").innerHTML = "Last Name should be at least 4 char long";
-    }else{
-        document.getElementById("lnameError").innerHTML ="";
-    }
+console.log(fName,lName,email,email);
 
-               if(!lName){
-        document.getElementById("emailError").innerHTML = "Email is required";
-    }else if(fName.length < 4){
-    document.getElementById("emailError").innerHTML = "Email should be at least 4 char long";
-    }else{
-        document.getElementById("emailError").innerHTML ="";
-    }
+if(!fName){
+    document.getElementById("fnameError").innerHTML = "First Name is Required!"
 
-               if(!lName){
-        document.getElementById("phoneError").innerHTML = "Phone No is required";
-    }else if(fName.length < 11){
-    document.getElementById("phoneError").innerHTML = "Phone Name should be at least 11 char long";
-    }else{
-        document.getElementById("phoneError").innerHTML ="";
-    }
+}
+else if(fName.length<4){
+    document.getElementById("fnameError").innerHTML = "First Name should be at least 4 character!"
+
+
+}
+else{
+    document.getElementById("fnameError").innerHTML="";
+}
+if(!lName){
+    document.getElementById("lnameError").innerHTML="Last Name is required."
+}
+else if(lName.length<4){
+    document.getElementById("lnameError").innerHTML = "at least 4 char"
+}
+else{
+    document.getElementById("lnameError").innerHTML =""
+}
+
+if(!email){
+    document.getElementById("emailError").innerHTML="email required"
+
+}
+else if(!email.includes("@")){
+    document.getElementById("emailError").innerHTML="@ must be givien"
+}
+else{
+    document.getElementById("emailError").innerHTML=""
+}
+
 
 
     return false;
-
-    
 }
